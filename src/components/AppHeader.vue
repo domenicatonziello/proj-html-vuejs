a3c7f5db0b302578688997748e3a117edc554f5f
domenicatonziello <domenica.tonziello10@gmail.com>
2023-01-31 17:43:21 +0100

    header completo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,11 @@
 <script>
+import ButtonVue from './generic/Button.vue';
 export default {
     name: 'AppHeader',
     props: {
         informations: Array,
-    }
+    },
+    components: { ButtonVue }
 }
 </script>
 
@@ -13,7 +15,7 @@ export default {
         <!-- header-top -->
         <div class="header-top d-flex justify-content-between align-items-center px-5">
             <div class="number">
-                <p> (001)88451234 88455438 </p>
+                <p class="m-0"> (001)88451234 88455438 </p>
             </div>
             <div class="social">
                 <font-awesome-icon icon="fa-brands fa-facebook-f" />
@@ -22,17 +24,20 @@ export default {
             </div>
         </div>
         <!-- navbar -->
-        <div class="navbar row d-flex justify-content-between align-items-center">
+        <div class="navbar px-5 d-flex justify-content-between align-items-center">
             <div id="logo" class="col-3">
-                <img src="../assets/images/cropped-Group-39-2x.png" alt="Phlox-Business-Logo">
+                <a href="#">
+                    <img src="../assets/images/cropped-Group-39-2x.png" alt="Phlox-Business-Logo">
+                </a>
             </div>
             <div class="menu col-6">
                 <ul class="d-flex justify-content-between ">
                     <li v-for="info in informations"> <a href="#">{{ info }}</a> </li>
                 </ul>
             </div>
-            <div class="shop col-3">
-                <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+            <div class="shop col-3 text-end">
+                <button-vue label="purchase"></button-vue>
+                <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="text-green" />
             </div>
         </div>
     </header>
@@ -42,8 +47,8 @@ export default {
 header {
     height: 140px;
     background-color: white;
-    border: 1px dashed black;
 
+    // header-top
     .header-top {
         height: 40px;
         color: white;
@@ -54,9 +59,9 @@ header {
         }
     }
 
+    // navbar
     .navbar {
         height: 100px;
-        padding: 5px 15px;
 
         img {
             max-width: 50%;
@@ -64,6 +69,7 @@ header {
 
         ul {
             list-style-type: none;
+            margin: 0;
 
             li {
                 a {
@@ -72,6 +78,14 @@ header {
                     text-transform: uppercase;
                 }
             }
+        }
+
+        .shop button {
+            background-image: linear-gradient(45deg, rgb(152, 225, 94), rgb(0, 217, 166));
+        }
+
+        .text-green {
+            color: rgb(0, 217, 166);
         }
     }
 }
---
+++ src/components/generic/Button.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'Button',
+    props: {
+        label: String,
+    }
+}
+</script>
+
+
+<template>
+    <button type="button"> {{ label }}</button>
+</template>
+
+
+<style scoped lang="scss">
+button {
+    margin: 15px;
+    padding: 13px 30px;
+    border-radius: 20px;
+    border: 1px solid transparent;
+    text-transform: uppercase;
+    color: white;
+    font-size: 11px;
+    box-shadow: 5px 2px 15px lightgray;
+}
+</style>
